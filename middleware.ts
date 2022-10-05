c2cdee5bbc94531a1e09f1b01691101ce818f117
cesine <cesine@yahoo.com>
2022-10-05 17:11:04 -0400

    convert to support  www.brand.com

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,5 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from 'next/server';
+import { BRANDS, isValidBrand } from '@lib/brand';
 
 export const config = {
   matcher: [
@@ -10,48 +11,70 @@ export const config = {
      * 4. /examples (inside /public)
      * 5. all root files inside /public (e.g. /favicon.ico)
      */
-    "/((?!api|_next|fonts|examples|[\\w-]+\\.\\w+).*)",
+    '/((?!api|_next|fonts|examples|[\\w-]+\\.\\w+).*)',
   ],
 };
 
 export default function middleware(req: NextRequest) {
   const url = req.nextUrl;
 
-  // Get hostname of request (e.g. demo.vercel.pub, demo.localhost:3000)
-  const hostname = req.headers.get("host") || "demo.vercel.pub";
+  // Get hostname of request (e.g. demo.ferme.ca, demo.localhost:3000)
+  const hostname = req.headers.get('host') || 'demo.ferme.ca';
 
   // Only for demo purposes - remove this if you want to use your root domain as the landing page
-  if (hostname === "vercel.pub" || hostname === "platforms.vercel.app") {
-    return NextResponse.redirect("https://demo.vercel.pub");
+  if (hostname === 'ferme.ca' || hostname === 'platforms.vercel.app') {
+    return NextResponse.redirect('https://demo.ferme.ca');
   }
 
-  /*  You have to replace ".vercel.pub" with your own domain if you deploy this example under your domain.
+  /*  You have to replace '.ferme.ca' with your own domain if you deploy this example under your domain.
       You can also use wildcard subdomains on .vercel.app links that are associated with your Vercel team slug
-      in this case, our team slug is "platformize", thus *.platformize.vercel.app works. Do note that you'll
-      still need to add "*.platformize.vercel.app" as a wildcard domain on your Vercel dashboard. */
-  const currentHost =
-    process.env.NODE_ENV === "production" && process.env.VERCEL === "1"
-      ? hostname
-          .replace(`.vercel.pub`, "")
-          .replace(`.platformize.vercel.app`, "")
-      : hostname.replace(`.localhost:3000`, "");
-  // rewrites for app pages
-  if (currentHost == "app") {
+      in this case, our team slug is 'platformize', thus *.platformize.vercel.app works. Do note that you'll
+      still need to add '*.platformize.vercel.app' as a wildcard domain on your Vercel dashboard. */
+  let currentHost =
+    hostname.includes('localhost')
+      ? hostname.replace(`.localhost:3000`, '')
+      : hostname
+          .replace(/www\.(.*)\.com/, '$1')
+          .replace(`.ferme.ca`, '')
+          .replace(`.platformize.vercel.app`, '')
+
+  if (!isValidBrand(currentHost)) {
+    console.warn(`${currentHost} is not a valid brand, overriding it to palmier`)
+    currentHost = 'palmier';
+  }
+
+  // rewrites for chene pages
+  if (currentHost === 'chene') {
     if (
-      url.pathname === "/login" &&
-      (req.cookies.get("next-auth.session-token") ||
-        req.cookies.get("__Secure-next-auth.session-token"))
+      url.pathname === '/login' &&
+      (req.cookies.get('next-auth.session-token') ||
+        req.cookies.get('__Secure-next-auth.session-token'))
     ) {
-      url.pathname = "/";
+      url.pathname = '/';
       return NextResponse.redirect(url);
     }
 
-    url.pathname = `/app${url.pathname}`;
+    url.pathname = `/chene${url.pathname}`;
+    return NextResponse.rewrite(url);
+  }
+
+  // rewrites for sapin pages?
+  if (currentHost === 'sapin') {
+    if (
+      url.pathname === '/login' &&
+      (req.cookies.get('next-auth.session-token') ||
+        req.cookies.get('__Secure-next-auth.session-token'))
+    ) {
+      url.pathname = '/';
+      return NextResponse.redirect(url);
+    }
+
+    url.pathname = `/sapin${url.pathname}`;
     return NextResponse.rewrite(url);
   }
 
   // rewrite root application to `/home` folder
-  if (hostname === "localhost:3000" || hostname === "platformize.vercel.app") {
+  if (hostname === 'localhost:3000' || hostname === 'platformize.vercel.app') {
     url.pathname = `/home${url.pathname}`;
     return NextResponse.rewrite(url);
   }
