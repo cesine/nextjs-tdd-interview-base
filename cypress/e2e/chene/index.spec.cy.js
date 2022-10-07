c8588183b3ca88474818f69213d95ebdebb6aad3
cesine <cesine@yahoo.com>
2022-10-07 14:29:49 -0400

    move 404 to root

--- cypress/e2e/chene/index.spec.cy.js
+++ cypress/e2e/chene/index.spec.cy.js
@@ -2,7 +2,7 @@ describe('home', () => {
   // TODO updating to react 18 exposed a bug
   // Hydration failed because the initial UI does not match what was rendered on the server.
   // This will be resolved when we move to platforms so skipping the test for now
-  it.skip("should render the home page and display a message", () => {
+  it("should render the home page and display a message", () => {
     cy.visit('http://localhost:3000', {
       headers: {
         host: 'www.chene.com',
